526b881927bb85677c388dba5686fbef719d54e4
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 16:58:40 +0300

    add observable return type

--- my-app/src/app/services/repo-service.service.ts
+++ my-app/src/app/services/repo-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Repository } from '../Repository';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class RepoService {
   private url: string = 'https://api.github.com/orgs/bosch-io/repos';
   constructor(private http: HttpClient) {}
 
-  getRepos(): Observable<any> {
-    return this.http.get<any>(this.url);
+  getRepos(): Observable<Repository[]> {
+    return this.http.get<Repository[]>(this.url);
   }
 }
