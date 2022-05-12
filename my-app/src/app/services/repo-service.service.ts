5ead0e1798465b9b3618126a4486f6f8c2b3cbfb
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 15:40:55 +0300

    create getRepos method

--- my-app/src/app/services/repo-service.service.ts
+++ my-app/src/app/services/repo-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RepoServiceService {
+  private url: string = 'https://api.github.com/orgs/bosch-io/repos';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getRepos(): Observable<any> {
+    return this.http.get<any>(this.url);
+  }
 }
