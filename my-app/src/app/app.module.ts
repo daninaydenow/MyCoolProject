d514f6fcd8dd6a502631bfdb60e178e0efcdb9fb
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 21:34:47 +0300

    Add new component and modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { DataGridComponent } from './components/data-grid/data-grid.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [AppComponent, DataGridComponent],
-  imports: [BrowserModule, MatTableModule, HttpClientModule],
+  declarations: [AppComponent, DataGridComponent, ToolbarComponent],
+  imports: [
+    BrowserModule,
+    MatTableModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
