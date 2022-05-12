60619b0a4f5ead689d5dbcbb163bc0023d63f6ff
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 15:22:37 +0300

    add HttpCliendModule

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-import { DataGridComponent } from './components/data-grid/data-grid.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { DataGridComponent } from './components/data-grid/data-grid.component';
 
 @NgModule({
   declarations: [AppComponent, DataGridComponent],
@@ -16,6 +17,7 @@ import { MatSortModule } from '@angular/material/sort';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
