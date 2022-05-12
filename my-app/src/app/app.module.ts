b119d6e15a947e5cdd43e309737c1b7bcdb2ebf5
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-12 16:56:05 +0300

    remove unnecessary modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -11,14 +9,7 @@ import { DataGridComponent } from './components/data-grid/data-grid.component';
 
 @NgModule({
   declarations: [AppComponent, DataGridComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, MatTableModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
