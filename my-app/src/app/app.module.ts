49c95541306e7aa40ccfcc5739b241eb11e79d4d
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-13 12:39:13 +0300

    import matsort and pagination modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 import { AppComponent } from './app.component';
 import { DataGridComponent } from './components/data-grid/data-grid.component';
@@ -16,12 +18,14 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
   declarations: [AppComponent, DataGridComponent, ToolbarComponent],
   imports: [
     BrowserModule,
-    MatTableModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    MatTableModule,
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
