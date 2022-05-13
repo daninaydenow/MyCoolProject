461d85b80b34f12c0c8fcf42bbde6f069b1c5a5a
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-13 11:31:35 +0300

    add MatTableDataSource and filter method

--- my-app/src/app/components/data-grid/data-grid.component.ts
+++ my-app/src/app/components/data-grid/data-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Repository } from 'src/app/Repository';
 import { RepoService } from 'src/app/services/repo-service.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-data-grid',
@@ -8,7 +9,7 @@ import { RepoService } from 'src/app/services/repo-service.service';
   styleUrls: ['./data-grid.component.css'],
 })
 export class DataGridComponent implements OnInit {
-  dataSource: Repository[] = [];
+  dataSource!: MatTableDataSource<Repository>;
 
   // Table columns header cell names
   displayedColumns: string[] = [
@@ -27,6 +28,13 @@ export class DataGridComponent implements OnInit {
 
   // Process the response and assign repos to dataSource
   getRepos(): void {
-    this.repoService.getRepos().subscribe((repos) => (this.dataSource = repos));
+    this.repoService
+      .getRepos()
+      .subscribe((repos) => (this.dataSource = new MatTableDataSource(repos)));
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
