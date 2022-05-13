7aab074564f8cb8a86eca8cc034c0e2ea4cabed3
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-13 11:31:47 +0300

    import necessary modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { DataGridComponent } from './components/data-grid/data-grid.component';
@@ -18,6 +20,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
     HttpClientModule,
     MatToolbarModule,
     MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
