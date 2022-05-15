292b95967c4b2d241446074573d9215d461c3e46
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-15 11:27:39 +0300

    import testmodule dependencies

--- my-app/src/app/services/repo.service.spec.ts
+++ my-app/src/app/services/repo.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RepoService } from './repo.service';
 
 describe('RepoService', () => {
   let service: RepoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RepoService],
+    });
     service = TestBed.inject(RepoService);
   });
 
